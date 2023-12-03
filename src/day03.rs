c3d5b964859583e823da9e8b6e31bcc45cb51e30
Mr.Robb <roberto.ariosa@est.fib.upc.edu>
2023-12-03 19:53:33 +0100

    Day 03

--- src/day03.rs
+++ src/day03.rs
@@ -1,9 +1,5 @@
 #![allow(clippy::must_use_candidate, clippy::missing_panics_doc)]
 
-// The engineer explains that an engine part seems to be missing from the engine, but nobody can figure out which one. If you can add up all the part numbers in the engine schematic, it should be easy to work out which part is missing.
-
-// The engine schematic (your puzzle input) consists of a visual representation of the engine. There are lots of numbers and symbols you don't really understand, but apparently any number adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum. (Periods (.) do not count as a symbol.)
-
 use std::{borrow::BorrowMut, collections::BTreeMap};
 
 use pathfinding::matrix::Matrix;
