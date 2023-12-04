ef16448b84099dac1f10bc8e230757629b90dc9b
Roberto Ariosa <roberto.ariosa@upc.edu>
2023-12-04 12:18:38 +0100

    Day 04: Extract common part

--- src/day04.rs
+++ src/day04.rs
@@ -1,50 +1,46 @@
-#![allow(clippy::must_use_candidate, clippy::missing_panics_doc)]
+#![allow(
+    clippy::must_use_candidate,
+    clippy::missing_panics_doc,
+    clippy::cast_possible_truncation
+)]
+
+fn get_points(line: &str) -> usize {
+    let (_, numbers) = line.split_once(": ").unwrap();
+    let (winning, yours) = numbers.split_once(" | ").unwrap();
+    let winning = winning
+        .split_whitespace()
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect::<Vec<_>>();
+    let yours = yours
+        .split_whitespace()
+        .map(|n| n.parse::<u64>().unwrap())
+        .collect::<Vec<_>>();
+    yours.iter().filter(|&number| winning.contains(number)).count()
+}
 
 pub fn part1(input: &str) -> u64 {
     input
         .lines()
-        .map(|line| {
-            let (_, numbers) = line.split_once(": ").unwrap();
-            let (winning, yours) = numbers.split_once(" | ").unwrap();
-            let winning = winning
-                .split_whitespace()
-                .map(|n| n.parse::<u64>().unwrap())
-                .collect::<Vec<_>>();
-            let yours = yours
-                .split_whitespace()
-                .map(|n| n.parse::<u64>().unwrap())
-                .collect::<Vec<_>>();
-            u32::try_from(yours.iter().filter(|&number| winning.contains(number)).count()).unwrap()
-        })
+        .map(get_points)
         .filter(|&points| points > 0)
-        .map(|points| 2u64.pow(points - 1))
+        .map(|points| 2u64.pow(points as u32 - 1))
         .sum()
 }
 
 pub fn part2(input: &str) -> u64 {
-    let how_many_cards =
-        input
-            .lines()
-            .enumerate()
-            .fold(vec![0; input.lines().count()], |mut how_many_cards, (card, line)| {
-                let (_, numbers) = line.split_once(": ").unwrap();
-                let (winning, yours) = numbers.split_once(" | ").unwrap();
-                let winning = winning
-                    .split_whitespace()
-                    .map(|n| n.parse::<u64>().unwrap())
-                    .collect::<Vec<_>>();
-                let yours = yours
-                    .split_whitespace()
-                    .map(|n| n.parse::<u64>().unwrap())
-                    .collect::<Vec<_>>();
-                let points = yours.iter().filter(|&number| winning.contains(number)).count();
-                let cards = how_many_cards[card];
-                how_many_cards[card + 1..card + 1 + points]
-                    .iter_mut()
-                    .for_each(|n| *n += cards + 1);
-                how_many_cards
-            });
-    how_many_cards.iter().sum::<u64>() + input.lines().count() as u64
+    input
+        .lines()
+        .map(get_points)
+        .enumerate()
+        .fold(vec![1; input.lines().count()], |mut how_many_cards, (card, points)| {
+            let cards = how_many_cards[card];
+            for n in &mut how_many_cards[card + 1..card + 1 + points] {
+                *n += cards;
+            }
+            how_many_cards
+        })
+        .iter()
+        .sum()
 }
 
 pub fn main() {
